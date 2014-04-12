4115797fb93ce7fdaf7a5662e478fb58c3c540c6
Archanciel <jp.schnyder@gmail.com>
2014-04-12 17:05:11 +0200

    add test cases and improved test data layout

--- TEST/com_dailystats/DailyStatsDaoTest.php
+++ TEST/com_dailystats/DailyStatsDaoTest.php
@@ -10,11 +10,14 @@ class DailyStatsDaoTest extends DailyStatsTestBase {
 		parent::setUp ();
 	}
 	
-	public function testGetLastAndTotalHitsAndDownloadsArr() {
+	/**
+	 * Tests 1 article with only 1 daily stats rec
+	 */
+	public function testGetLastAndTotalHitsAndDownloadsArr_1_dailyStats() {
 		$res = DailyStatsDao::getLastAndTotalHitsAndDownloadsArr(CHART_MODE_ARTICLE,1);
-		
+	
 		$this->assertEquals(5, count($res),'count($res)');
-		
+	
 		$this->assertEquals('20-10',$res[DATE_IDX],'date');
 		$this->assertEquals(15,$res[LAST_HITS_IDX],'date hits');
 		$this->assertEquals(150,$res[TOTAL_HITS_IDX],'total hits');
@@ -22,6 +25,31 @@ class DailyStatsDaoTest extends DailyStatsTestBase {
 		$this->assertEquals(100,$res[TOTAL_DOWNLOADS_IDX],'total downloads');
 	}
 	
+	/**
+	 * Tests 1 article with only 2 daily stats recs
+	 */
+	public function testGetLastAndTotalHitsAndDownloadsArr_2_dailyStats() {
+		$res = DailyStatsDao::getLastAndTotalHitsAndDownloadsArr(CHART_MODE_ARTICLE,2);
+	
+		$this->assertEquals(5, count($res),'count($res)');
+	
+		$this->assertEquals('21-11',$res[DATE_IDX],'date');
+		$this->assertEquals(150,$res[LAST_HITS_IDX],'date hits');
+		$this->assertEquals(1500,$res[TOTAL_HITS_IDX],'total hits');
+		$this->assertEquals(100,$res[LAST_DOWNLOADS_IDX],'date downloads');
+		$this->assertEquals(1000,$res[TOTAL_DOWNLOADS_IDX],'total downloads');
+	}
+	
+	/**
+	 * Tests 1 article with no daily stats rec
+	 */
+	public function testGetLastAndTotalHitsAndDownloadsArr_no_dailyStats() {
+		$res = DailyStatsDao::getLastAndTotalHitsAndDownloadsArr(CHART_MODE_ARTICLE,3);
+		
+		$this->assertEquals(5, count($res),'count($res)');
+		
+		$this->assertNull($res[DATE_IDX],'date');
+	}
 	/**
 	 * Gets the data set to be loaded into the database during setup
 	 *
