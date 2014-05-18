e6110abb335059495285d8d3abd2ab2140813097
Archanciel <jp.schnyder@gmail.com>
2014-05-18 16:12:28 +0200

    20 + 21 days gap texting

--- TEST/com_dailystats/unittest/DailyStatsDaoExecDailyStatsCronOneDailyStatsRecTwoArticlsTest.php
+++ TEST/com_dailystats/unittest/DailyStatsDaoExecDailyStatsCronOneDailyStatsRecTwoArticlsTest.php
@@ -119,6 +119,90 @@ class DailyStatsDaoExecDailyStatsCronOneDailyStatsRecTwoArticlesTest extends Dai
 		$this->checkEntryExistInLog("Stats for $today added in DB. 0 rows inserted for new attachment\(s\). 2 rows inserted for existing attachments \(gap filled: 2 day\(s\)\).");
 	}
 	
+	
+	/**
+	 * Tests daily stats rec generation for 2 published article and 1 unpublished article, with 1 
+	 * attachment each, in a daily stats table with 1 daily stat rec for each published article dated 
+	 * 20 days before cron execution.
+	 */
+	public function testExecDailyStatsCronOneDailyStatsRecTwoArticles20DaysInterval() {
+		// force existing daily stats rec date to yesterday
+		
+		$yesterday = date("Y-m-d",strtotime("-20 day"));
+  		$this->updateAllDailyStatRec($yesterday);
+		
+  		// execute cron
+  		
+ 		DailyStatsDao::execDailyStatsCron("#__" . $this->daily_stats_table_name,"#__attachments_cron_test","#__content_cron_test");
+		
+ 		// verify results
+ 		
+		/* @var $db JDatabase */
+    	$db = JFactory::getDBO();
+		$query = "SELECT COUNT(id) FROM #__" . $this->daily_stats_table_name; 
+    	$db->setQuery($query);
+    	$count = $db->loadResult();
+
+		$this->assertEquals(4,$count,'4 daily_stats records expected, 2 for yesterday and 2 for today');
+
+		$today = date("Y-m-d",strtotime("now"));
+
+		// check daily stats for article 1
+		
+		$query = "SELECT * FROM #__" . $this->daily_stats_table_name . " WHERE article_id = 1 AND date = '$today'"; 
+    	$db->setQuery($query);
+    	$res = $db->loadAssoc();
+		
+		$this->assertEquals(11,$res['date_hits'],'date hits');
+		$this->assertEquals(111,$res['total_hits_to_date'],'total hits');
+		$this->assertEquals(1,$res['date_downloads'],'date downloads');
+		$this->assertEquals(11,$res['total_downloads_to_date'],'total downloads');
+
+		// check daily stats for article 2
+		
+		$query = "SELECT * FROM #__" . $this->daily_stats_table_name . " WHERE article_id = 2 AND date = '$today'"; 
+    	$db->setQuery($query);
+    	$res = $db->loadAssoc();
+		
+		$this->assertEquals(2,$res['date_hits'],'date hits');
+		$this->assertEquals(112,$res['total_hits_to_date'],'total hits');
+		$this->assertEquals(1,$res['date_downloads'],'date downloads');
+		$this->assertEquals(12,$res['total_downloads_to_date'],'total downloads');
+
+		$this->checkEntryExistInLog("Stats for $today added in DB. 0 rows inserted for new attachment\(s\). 2 rows inserted for existing attachments \(gap filled: 20 day\(s\)\).");
+	}
+	
+	
+	/**
+	 * Tests daily stats rec generation for 2 published article and 1 unpublished article, with 1 
+	 * attachment each, in a daily stats table with 1 daily stat rec for each published article dated 
+	 * 21 days before cron execution.
+	 */
+	public function testExecDailyStatsCronOneDailyStatsRecTwoArticles21DaysInterval() {
+		// force existing daily stats rec date to yesterday
+		
+		$yesterday = date("Y-m-d",strtotime("-21 day"));
+  		$this->updateAllDailyStatRec($yesterday);
+		
+  		// execute cron
+  		
+ 		DailyStatsDao::execDailyStatsCron("#__" . $this->daily_stats_table_name,"#__attachments_cron_test","#__content_cron_test");
+		
+ 		// verify results
+ 		
+		/* @var $db JDatabase */
+    	$db = JFactory::getDBO();
+		$query = "SELECT COUNT(id) FROM #__" . $this->daily_stats_table_name; 
+    	$db->setQuery($query);
+    	$count = $db->loadResult();
+
+		$this->assertEquals(2,$count,'2 daily_stats records expected, two for yesterday and none for today');
+
+		$today = date("Y-m-d",strtotime("now"));
+
+		$this->checkEntryExistInLog("Stats for $today added in DB. 0 rows inserted for new attachment\(s\). 0 rows inserted for existing attachments. GAP EXCEEDS MAX INTERVAL OF 20 DAYS !");
+	}
+	
 	private function updateAllDailyStatRec($forDate) {
 		$query= "UPDATE jos_" . $this->daily_stats_table_name .
 				" SET date = '$forDate'";
