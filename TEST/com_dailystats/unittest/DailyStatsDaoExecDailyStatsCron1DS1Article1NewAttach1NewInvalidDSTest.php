980c6fba682a6c71659a2319833808ea2ee81d0a
Archanciel <jp.schnyder@gmail.com>
2014-05-22 00:32:51 +0200

    improve documentatin

--- TEST/com_dailystats/unittest/DailyStatsDaoExecDailyStatsCron1DS1Article1NewAttach1NewInvalidDSTest.php
+++ TEST/com_dailystats/unittest/DailyStatsDaoExecDailyStatsCron1DS1Article1NewAttach1NewInvalidDSTest.php
@@ -5,9 +5,12 @@ require_once COM_DAILYSTATS_PATH . '..\dao\dailyStatsDao.php';
 require_once COM_DAILYSTATS_PATH . '..\dailyStatsConstants.php';
 
 /**
- * This class tests daily stats generation for a db containing 1 published article having one attachment,
- * in a daily stats rec table containing one daily stats rec for the published article, dated at 
- * yesterday.
+ * This class tests daily stats generation for a db in an invalid state. Article 1 had initialy
+ * attachment 10001 and a valid ds rec for attachment 10001. Then, attachment 10002 was added for
+ * article 1 and a ds rec was manually created, but with a total download count to date >
+ * than the download count of the new attachment. This causes a SQL error and a failure of the
+ * daily stats generation process. The class ensures this bug is now handled gracefully (entry
+ * in log file and email sending in prod environment).
  *  
  * @author Jean-Pierre
  *
@@ -19,7 +22,7 @@ class DailyStatsDaoExecDailyStatsCron1DS1Article1NewAttach1NewInvalidDSTest exte
 	 * Tests daily stats rec generation for 1 article with 1 attachment in a daily stats table
 	 * with 1 daily stat rec dated 1 day before cron execution.
 	 */
-	public function testExecDailyStatsCronOneDailyStatsRecOneArticleOneDayInterval() { 
+	public function testExecDailyStatsCronFailureOneDailyStatsRecOneArticleOneDayInterval() { 
   		// update first DS rec date
 		$this->updateDailyStatRec(1,date("Y-m-d",strtotime("-2 day")));
   		
